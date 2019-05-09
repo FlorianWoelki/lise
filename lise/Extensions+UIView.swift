e78c0f8cf73984f3f05ffe0bcc17d5a4c4c97369
FlorianWoelki <FlorianWoelki@gmx.de>
2019-05-09 14:32:46 +0200

    added basic twitter design layout

---
+++ lise/Extensions+UIView.swift
@@ -0,0 +1,62 @@
+//
+//  Extensions+UIView.swift
+//  lise
+//
+//  Created by Florian Woelki on 09.05.19.
+//  Copyright © 2019 Florian Woelki. All rights reserved.
+//
+
+import UIKit
+
+extension UIView {
+    
+    func anchor(top: NSLayoutYAxisAnchor?, leading: NSLayoutXAxisAnchor?, bottom: NSLayoutYAxisAnchor?, trailing: NSLayoutXAxisAnchor?, padding: UIEdgeInsets = .zero, size: CGSize = .zero) {
+        
+        translatesAutoresizingMaskIntoConstraints = false
+        
+        if let top = top {
+            self.topAnchor.constraint(equalTo: top, constant: padding.top).isActive = true
+        }
+        
+        if let leading = leading {
+            self.leadingAnchor.constraint(equalTo: leading, constant: padding.left).isActive = true
+        }
+        
+        if let bottom = bottom {
+            self.bottomAnchor.constraint(equalTo: bottom, constant: -padding.bottom).isActive = true
+        }
+        
+        if let trailing = trailing {
+            self.trailingAnchor.constraint(equalTo: trailing, constant: -padding.right).isActive = true
+        }
+        
+        if size.width != 0 {
+            self.widthAnchor.constraint(equalToConstant: size.width).isActive = true
+        }
+        
+        if size.height != 0 {
+            self.heightAnchor.constraint(equalToConstant: size.height).isActive = true
+        }
+        
+    }
+    
+    func fillSuperview(padding: UIEdgeInsets = .zero) {
+        translatesAutoresizingMaskIntoConstraints = false
+        if let superviewTopAnchor = superview?.topAnchor {
+            topAnchor.constraint(equalTo: superviewTopAnchor, constant: padding.top).isActive = true
+        }
+        
+        if let superviewBottomAnchor = superview?.bottomAnchor {
+            bottomAnchor.constraint(equalTo: superviewBottomAnchor, constant: -padding.bottom).isActive = true
+        }
+        
+        if let superviewLeadingAnchor = superview?.leadingAnchor {
+            leadingAnchor.constraint(equalTo: superviewLeadingAnchor, constant: padding.left).isActive = true
+        }
+        
+        if let superviewTrailingAnchor = superview?.trailingAnchor {
+            trailingAnchor.constraint(equalTo: superviewTrailingAnchor, constant: -padding.right).isActive = true
+        }
+    }
+    
+}
--- lise/TwitterClone/Views/TweetCell.swift
+++ lise/TwitterClone/Views/TweetCell.swift
@@ -10,10 +10,67 @@ import UIKit
 
 class TweetCell: UICollectionViewCell {
     
+    private let topPadding: CGFloat = 24
+    
+    private let profileImageView: UIImageView = {
+        let iv = UIImageView()
+        iv.backgroundColor = .red
+        iv.layer.cornerRadius = 50 / 2
+        iv.clipsToBounds = true
+        return iv
+    }()
+    
+    private let profileDetailsLabel: UILabel = {
+        let label = UILabel()
+        
+        let attributedText = NSMutableAttributedString(attributedString: NSAttributedString(string: "Fullname ", attributes: [.font: UIFont.boldSystemFont(ofSize: 16)]))
+        attributedText.append(NSAttributedString(string: "@flowy • 21h", attributes: [.font: UIFont.systemFont(ofSize: 16, weight: .ultraLight)]))
+        
+        label.attributedText = attributedText
+        
+        return label
+    }()
+    
+    private let contentTextLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et."
+        label.numberOfLines = 0
+        return label
+    }()
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         
-        backgroundColor = .red
+        backgroundColor = .white
+    
+        setupLayout()
+    }
+    
+    private func setupLayout() {
+        
+        // Anchor profile image view
+        addSubview(profileImageView)
+        profileImageView.anchor(top: topAnchor, leading: leadingAnchor, bottom: nil, trailing: nil, padding: .init(top: topPadding, left: 32, bottom: 0, right: 0))
+        profileImageView.widthAnchor.constraint(equalToConstant: 50).isActive = true
+        profileImageView.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        
+        // Construct full stackView
+        let fullStackView = UIStackView(arrangedSubviews: [
+            profileDetailsLabel, contentTextLabel
+            ])
+        fullStackView.spacing = 4
+        fullStackView.axis = .vertical
+        
+        addSubview(fullStackView)
+        
+        fullStackView.anchor(top: topAnchor, leading: profileImageView.trailingAnchor, bottom: nil, trailing: trailingAnchor, padding: .init(top: topPadding, left: 16, bottom: 0, right: 24))
+        
+        // Construct seperator view
+        let seperatorView = UIView()
+        seperatorView.backgroundColor = #colorLiteral(red: 0.6688795337, green: 0.6688795337, blue: 0.6688795337, alpha: 1)
+        addSubview(seperatorView)
+        seperatorView.anchor(top: nil, leading: leadingAnchor, bottom: bottomAnchor, trailing: trailingAnchor)
+        seperatorView.heightAnchor.constraint(equalToConstant: 1).isActive = true
     }
     
     required init?(coder aDecoder: NSCoder) {
